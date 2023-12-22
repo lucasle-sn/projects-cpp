66072c8b95fa3bf6e2965f11ea507c6f81b30e25
lucasle-sn <lqtrung1993@gmail.com>
2023-12-22 18:20:00 +1100

    Fix clang-format

--- coding_problems/amstrong_number/main.cc
+++ coding_problems/amstrong_number/main.cc
@@ -1,91 +1,92 @@
+#include <errno.h>
+#include <array>
+#include <cmath>
 #include <cstdio>
 #include <cstdlib>
-#include <errno.h>
-#include <cmath>
-#include <array>
 
 /**
  * @brief Check if a number is Amstrong number
  * 
- * An Amstrong number is a number that is equal sum of each digit powered by the number of digits.
- * For example: 
- *  [abc = a^3 + b^3 + c^3]
- *  [abcd = a^4 + b^4 + c^4 + d^4]
- * 
+ * An Amstrong number is a number that is equal sum of each digit powered by the
+ * number of digits. For example: [abc = a^3 + b^3 + c^3] [abcd = a^4 + b^4 +
+ * c^4 + d^4]
+ *
  * @param number Number that need checked
  * @return bool
  */
 bool is_amstrong_number(int number) {
-    if (number <= 0) {
-        return false;
-    }
+  if (number <= 0) {
+    return false;
+  }
 
-    auto original_number = number;
+  auto original_number = number;
 
-    std::array<u_int8_t, 100> digits {0};
-    size_t digit_idx = 0;    
-    while (number > 0) {
-        digits[digit_idx] = number % 10;
-        digit_idx++;
-        number = static_cast<int>(number/10);
-    }
+  std::array<u_int8_t, 100> digits{0};
+  size_t digit_idx = 0;
+  while (number > 0) {
+    digits[digit_idx] = number % 10;
+    digit_idx++;
+    number = static_cast<int>(number / 10);
+  }
 
-    int sum = 0;
-    for (size_t i = 0; i < digit_idx; i++) {
-        sum += pow(digits[i], digit_idx);
-    }
+  int sum = 0;
+  for (size_t i = 0; i < digit_idx; i++) {
+    sum += pow(digits[i], digit_idx);
+  }
 
-    return (original_number == sum ? true : false);
+  return (original_number == sum ? true : false);
 }
 
 /**
  * @brief Parse integer parameter
- * 
+ *
  * @param args argument
  * @param end_args remaining argument
  * @param out Output value
- * @return bool 
+ * @return bool
  */
-bool integer_argparsing(const char * args, char * end_args, int& out) {
-    int number = static_cast<int>(std::strtol(args, &end_args, 0));
+bool integer_argparsing(const char *args, char *end_args, int &out) {
+  int number = static_cast<int>(std::strtol(args, &end_args, 0));
 
-    if (args == end_args) {
-        fprintf(stderr, "Number : %d Invalid (no digits found, 0 returned)\n", number);
-        return false;
-    } else if (errno == ERANGE) {
-        fprintf(stderr, "Number : %d Invalid (Overflow/Underflow occured)\n", number);
-        return false;
-    } else if (errno != 0 && number == 0) {
-        printf ("Number : %d Invalid (unspecified error occurred)\n", number);
-        return false;
-    } else if (errno == 0 && end_args && *end_args != 0) {
-        printf ("Number : %d Valid (but additional characters remain)\n", number);
-        return false;
-    }
+  if (args == end_args) {
+    fprintf(stderr, "Number : %d Invalid (no digits found, 0 returned)\n",
+            number);
+    return false;
+  } else if (errno == ERANGE) {
+    fprintf(stderr, "Number : %d Invalid (Overflow/Underflow occured)\n",
+            number);
+    return false;
+  } else if (errno != 0 && number == 0) {
+    printf("Number : %d Invalid (unspecified error occurred)\n", number);
+    return false;
+  } else if (errno == 0 && end_args && *end_args != 0) {
+    printf("Number : %d Valid (but additional characters remain)\n", number);
+    return false;
+  }
 
-    out = number;
-    return true;
+  out = number;
+  return true;
 }
 
-int main(int argc, char** argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Exact one integer required\n");
-        return EXIT_FAILURE;
-    }
-    
-    /* reset errno to 0 before call */
-    errno = 0;
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Exact one integer required\n");
+    return EXIT_FAILURE;
+  }
 
-    int number {0};
-    char *endptr {nullptr};
-    if (!integer_argparsing(argv[1], endptr, number)) {
-        return EXIT_FAILURE;
-    }
+  /* reset errno to 0 before call */
+  errno = 0;
 
-    if (is_amstrong_number(number)) {
-        fprintf(stdout, "%d is Amstrong number\n", number);
-    } else {
-        fprintf(stdout, "%d is not Amstrong number\n", number);
-    }
-    return EXIT_SUCCESS;
+  int number{0};
+  char *endptr{nullptr};
+  if (!integer_argparsing(argv[1], endptr, number)) {
+    return EXIT_FAILURE;
+  }
+
+  if (is_amstrong_number(number)) {
+    fprintf(stdout, "%d is Amstrong number\n", number);
+  } else {
+    fprintf(stdout, "%d is not Amstrong number\n", number);
+  }
+  return EXIT_SUCCESS;
 }
--- multithreading_interrupt/main.cc
+++ multithreading_interrupt/main.cc
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <pthread.h>
-#include <unistd.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <atomic>
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -12,74 +12,71 @@ std::atomic<bool> status{false};
 std::atomic<bool> runnable{true};
 
 void change_status(const char *thread, bool signal) {
-    printf("%s, change status: %s -> %s\n", thread, status ? "true" : "false", status ? "false" : "true");
-    status = signal;
-    printf("Signaling %s\n", signal ? "\"true\"" : "\"false\"");
-    pthread_cond_signal(&condition_cond);
+  printf("%s, change status: %s -> %s\n", thread, status ? "true" : "false",
+         status ? "false" : "true");
+  status = signal;
+  printf("Signaling %s\n", signal ? "\"true\"" : "\"false\"");
+  pthread_cond_signal(&condition_cond);
 }
 
-void set_status(const char *thread) {
-    change_status(thread, true);
-}
+void set_status(const char *thread) { change_status(thread, true); }
 
-void reset_status(const char *thread) {
-    change_status(thread, false);
-}
+void reset_status(const char *thread) { change_status(thread, false); }
 
 bool get_status(const char *thread) {
-    if (thread != NULL) {
-        printf("%s: status = %s\n", thread, status ? "true" : "false");
-    }
-    return status;
+  if (thread != NULL) {
+    printf("%s: status = %s\n", thread, status ? "true" : "false");
+  }
+  return status;
 }
 
 void wait_until_status(bool signal) {
-    pthread_mutex_lock(&condition_mutex);
-    if (status != signal) {
-        pthread_cond_wait(&condition_cond, &condition_mutex);
-    }
-    pthread_mutex_unlock(&condition_mutex);
+  pthread_mutex_lock(&condition_mutex);
+  if (status != signal) {
+    pthread_cond_wait(&condition_cond, &condition_mutex);
+  }
+  pthread_mutex_unlock(&condition_mutex);
 }
 
 void *run_main(void *arg) {
-    (void) arg;
-    const char *name = "MAIN thread";
-    while (runnable) {
-        printf("----- Main thread: ------>\n");
-        set_status(name);
+  (void)arg;
+  const char *name = "MAIN thread";
+  while (runnable) {
+    printf("----- Main thread: ------>\n");
+    set_status(name);
 
-        wait_until_status(false);
+    wait_until_status(false);
 
-        printf("%s: Doing something\n", name);
-    }
-    return nullptr;
+    printf("%s: Doing something\n", name);
+  }
+  return nullptr;
 }
 
 void *run_interrupt(void *arg) {
-    (void) arg;
-    const char *name = "INTERRUPT thread";
-    int count = 0;
+  (void)arg;
+  const char *name = "INTERRUPT thread";
+  int count = 0;
 
-    while (runnable) {
-        wait_until_status(true);
+  while (runnable) {
+    wait_until_status(true);
 
-        printf("%s: Doing something\n", name);
-        reset_status(name);
+    printf("%s: Doing something\n", name);
+    reset_status(name);
 
-        count++;
-        if (count >= 5) {
-            runnable = false;
-        }
+    count++;
+    if (count >= 5) {
+      runnable = false;
     }
-    return nullptr;
+  }
+  return nullptr;
 }
 
 int main() {
-    pthread_t thread_main, thread_interrupt;
+  pthread_t thread_main, thread_interrupt;
 
-    pthread_create(&thread_main, NULL, &run_main, NULL);
-    pthread_create(&thread_interrupt, NULL, &run_interrupt, NULL);
+  pthread_create(&thread_main, NULL, &run_main, NULL);
+  pthread_create(&thread_interrupt, NULL, &run_interrupt, NULL);
 
-    pthread_join(thread_main, NULL);
-    pthread_join(thread_interrupt, NULL);
+  pthread_join(thread_main, NULL);
+  pthread_join(thread_interrupt, NULL);
 }
