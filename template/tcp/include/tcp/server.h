48a2db4dc303fdca61903efe56d5139b80a4b863
lucasle-sn <lqtrung1993@gmail.com>
2024-06-27 22:52:55 +1000

    Add TCP server

---
+++ template/tcp/include/tcp/server.h
@@ -0,0 +1,88 @@
+#ifndef TEMPLATE_TCP_SERVER_H
+#define TEMPLATE_TCP_SERVER_H
+
+#include <public_types/error_codes.h>
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <unistd.h>
+#include <thread>
+#include <vector>
+
+namespace unimelb {
+
+class Server {
+ public:
+  /**
+   * @brief Delete default constructor
+   */
+  Server() = delete;
+
+  /**
+   * @brief Delete copy constructor
+   */
+  Server(const Server &) = delete;
+
+  /**
+   * @brief Delete move constructor
+   */
+  Server(Server &&) = delete;
+
+  /**
+   * @brief Delete copy assignment
+   */
+  Server &operator=(const Server &) = delete;
+
+  /**
+   * @brief Delete move assignment
+   */
+  Server &operator=(Server &&) = delete;
+
+  /**
+   * @brief Constructor
+   * @param port Server port
+   */
+  explicit Server(int port);
+
+  /**
+   * @brief Destructor
+   */
+  ~Server() { stop(); };
+
+  /**
+   * @brief Initialize TCP server
+   * @return ErrorCodes
+   */
+  ErrorCodes init();
+
+  /**
+   * @brief Main process TCP server
+   * @return ErrorCodes
+   */
+  ErrorCodes run();
+
+  /**
+   * @brief Stop server and close
+   */
+  void stop();
+
+ private:
+  /**
+   * @brief Handle client connection
+   * @param socket Client
+   */
+  void handleClient(int socket);
+
+  int fd_;    // File descriptor
+  int port_;  // Port
+  struct sockaddr_in address_;
+  size_t addrlen_;
+  bool isRunning_{false};             // Running status
+  std::vector<std::thread> threads_;  // Client threads
+};
+
+}  // namespace unimelb
+
+#endif  // TEMPLATE_TCP_SERVER_H
---
+++ template/tcp/src/server.cc
@@ -0,0 +1,85 @@
+#include <tcp/server.h>
+
+#include <cstring>
+#include <iostream>
+
+const int cBufferSize = 1024;
+
+namespace unimelb {
+
+Server::Server(int port) : port_(port), addrlen_(sizeof(address_)) {}
+
+ErrorCodes Server::init() {
+  fd_ = socket(AF_INET, SOCK_STREAM, 0);
+  if (fd_ == -1) {
+    fprintf(stderr, "Socket setup failed\n");
+    return ErrorCodes::ERROR;
+  }
+
+  int opt = 1;
+  if (setsockopt(fd_, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
+                 sizeof(opt)) == -1) {
+    fprintf(stderr, "Socket setup failed\n");
+    return ErrorCodes::ERROR;
+  }
+  address_.sin_family = AF_INET;
+  address_.sin_addr.s_addr = INADDR_ANY;
+  address_.sin_port = htons(port_);
+
+  if (bind(fd_, (struct sockaddr *)&address_, sizeof(address_)) < 0) {
+    fprintf(stderr, "Socket binding failed\n");
+    return ErrorCodes::ERROR;
+  }
+
+  return ErrorCodes::SUCCESS;
+}
+
+void Server::stop() {
+  if (!isRunning_) {
+    return;
+  }
+
+  isRunning_ = false;
+  close(fd_);
+
+  for (auto &thread : threads_) {
+    if (thread.joinable()) {
+      thread.join();
+    }
+  }
+}
+
+ErrorCodes Server::run() {
+  if (listen(fd_, 10) < 0) {
+    fprintf(stderr, "Error socket listening\n");
+    return ErrorCodes::ERROR;
+  }
+  isRunning_ = true;
+
+  while (isRunning_) {
+    int socket =
+        accept(fd_, (struct sockaddr *)&address_, (socklen_t *)&addrlen_);
+    if (socket < 0) {
+      fprintf(stderr, "Accept failed\n");
+      continue;
+    }
+
+    threads_.emplace_back(&handleClient, this, socket);
+  }
+
+  return ErrorCodes::SUCCESS;
+}
+
+void Server::handleClient(int socket) {
+  char buffer[cBufferSize] = {0};
+  read(socket, buffer, sizeof(buffer));
+  std::cout << "Message received: " << buffer << std::endl;
+
+  std::string hello = "Hello from server";
+  send(socket, hello.c_str(), hello.size(), 0);
+  std::cout << "Hello message sent" << std::endl;
+
+  close(socket);
+}
+
+}  // namespace unimelb
