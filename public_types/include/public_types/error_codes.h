8379e6ff8e7a2035d7e659da098cdc0a855d2af6
lucasle-sn <lqtrung1993@gmail.com>
2024-06-27 22:52:33 +1000

    Add error codes

---
+++ public_types/include/public_types/error_codes.h
@@ -0,0 +1,13 @@
+#ifndef PUBLIC_TYPES_ERROR_CODES_H
+#define PUBLIC_TYPES_ERROR_CODES_H
+
+namespace unimelb {
+
+enum class ErrorCodes {
+  SUCCESS,
+  ERROR,
+};
+
+}
+
+#endif  // PUBLIC_TYPES_ERROR_CODES_H
