d5603eda3120a9651cd2b83b680dcb2bb7c53188
Lucas Le <97814268+lucasle-sn@users.noreply.github.com>
2024-12-12 23:37:36 +1100

    Add error codes (#50)
    * Add error code header
    
    * Add unit tests
    
    * Add alias all for all-gcc

---
+++ public_types/include/public_types/error_codes.h
@@ -0,0 +1,41 @@
+#ifndef PUBLIC_TYPES_ERROR_CODES_H
+#define PUBLIC_TYPES_ERROR_CODES_H
+
+namespace qle {
+
+/**
+ * @brief ErrorCode
+ */
+enum class ErrorCode {
+  SUCCESS,  ///< Success
+  ERROR,    ///< Error
+  INVALID,  ///< Invalid
+};
+
+/**
+ * @brief ErrorCode Handler
+ */
+class ErrorCodeHandler {
+ public:
+  /**
+   * @brief Convert ErrorCode to string
+   *
+   * @param code ErrorCode
+   * @return const char *
+   */
+  static const char *error_code_to_string(ErrorCode code) {
+    switch (code) {
+      case ErrorCode::SUCCESS:
+        return "Success";
+      case ErrorCode::ERROR:
+        return "Error";
+      case ErrorCode::INVALID:
+      default:
+        return "Invalid";
+    }
+  }
+};
+
+}  // namespace qle
+
+#endif  // PUBLIC_TYPES_ERROR_CODES_H
